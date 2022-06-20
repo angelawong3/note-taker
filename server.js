c08fbef7d76b08851cc1bc3c78580503f2729333
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-20 20:56:32 +0100

    set up basic server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+// use port from heroku or 3001
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
